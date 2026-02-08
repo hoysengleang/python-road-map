4363db0e3e01ef5f2d8f95020595ae83d908934d
Leang <hoysengleang617@gmail.com>
2026-02-08 13:52:19 +0700

    hehee

---
+++ src/exercise/Decorators/Property.py
@@ -0,0 +1,17 @@
+class Animal:
+
+    def __init__(self,name):
+
+        self.name = name
+
+    @property
+    def display_name(self):
+        return self.name
+
+
+
+
+
+if __name__ == "__main__":
+    animal = Animal("Buddy")
+    print(animal.display_name)
