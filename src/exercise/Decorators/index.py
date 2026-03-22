2bbd2235326f8772dab8f810f63bbc3b724d8845
Leang <hoysengleang617@gmail.com>
2026-03-22 14:18:09 +0700

    update

--- src/exercise/Decorators/index.py
+++ src/exercise/Decorators/index.py
@@ -1,5 +1,6 @@
 def my_decorator(func):
     def wrapper():
+        
         print("Something is happening before the function is called.")
         func()
         print("Something is happening after the function is called.")
@@ -7,6 +8,7 @@ def my_decorator(func):
 
 @my_decorator
 def say_hello():
+
     print("Hello!")
 
 say_hello()
